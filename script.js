a7ae1ef440886d01bf3bbc70ce80554e45af2e11
Luke Renoe <lukerenoe@gmail.com>
2022-05-16 15:48:36 -0600

    monday

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ $("#saveBtn").on("click", function () {
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, text);
 }) 
+
+
+
+    // Get item from local storage if any
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+
+
+
+
