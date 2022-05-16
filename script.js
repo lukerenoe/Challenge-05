a4ef00a20f183bd39fce5f3797961614aa885172
Luke Renoe <lukerenoe@gmail.com>
2022-05-15 21:10:44 -0600

    oof

--- script.js
+++ script.js
@@ -1,76 +1,18 @@
 var time = moment();
-var save = 
-
 
 function displayTime() {
-$("#currentDay").text("It's fucking " + time.format('dddd, MMM Do YYYY'));
+$("#currentDay").text("It's fucking " + time.format('dddd, MMMM Do YYYY, h:mm:ss a'));
 setTimeout(displayTime, 1000);
 }
 
+
 $(document).ready(function() {
     displayTime();
+$("#saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+}) 
 });
 
 
-$("#saveBtn").click (function (event) {
-    event.preventDefault();
-    var text = document.querySelector("#9").value;
-    localStorage.setItem("9am", text);
-});
-
-
-
-
-// $(document).ready(function () {
-//     // saveBtn click listener 
-//     $(".saveBtn").on("click", function () {
-//         // Get nearby values of the description in JQuery
-//         var text = $(this).siblings(".description").val();
-//         var time = $(this).parent().attr("id");
-
-//         // Save text in local storage
-//         localStorage.setItem(time, text);
-//     })
-
-//     function timeTracker() {
-//         //get current number of hours.
-//         var timeNow = moment().hour();
-
-//         // loop over time blocks
-//         $(".time-block").each(function () {
-//             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-//             // To check the time and add the classes for background indicators
-//             if (blockTime < timeNow) {
-//                 $(this).removeClass("future");
-//                 $(this).removeClass("present");
-//                 $(this).addClass("past");
-//             }
-//             else if (blockTime === timeNow) {
-//                 $(this).removeClass("past");
-//                 $(this).removeClass("future");
-//                 $(this).addClass("present");
-//             }
-//             else {
-//                 $(this).removeClass("present");
-//                 $(this).removeClass("past");
-//                 $(this).addClass("future");
-
-//             }
-//         })
-//     }
-
-//     // Get item from local storage if any
-//     $("#8 .description").val(localStorage.getItem("8"));
-//     $("#9 .description").val(localStorage.getItem("9"));
-//     $("#10 .description").val(localStorage.getItem("10"));
-//     $("#11 .description").val(localStorage.getItem("11"));
-//     $("#12 .description").val(localStorage.getItem("12"));
-//     $("#13 .description").val(localStorage.getItem("13"));
-//     $("#14 .description").val(localStorage.getItem("14"));
-//     $("#15 .description").val(localStorage.getItem("15"));
-//     $("#16 .description").val(localStorage.getItem("16"));
-//     $("#17 .description").val(localStorage.getItem("17"));
-
-//     timeTracker();
-// })
