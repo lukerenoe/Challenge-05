e108321558fa6ab3ed37727d63b70656781f87aa
Luke Renoe <lukerenoe@gmail.com>
2022-05-16 16:07:50 -0600

    monday

--- script.js
+++ script.js
@@ -5,18 +5,13 @@ $("#currentDay").text("It's fucking " + time.format('dddd, MMMM Do YYYY, h:mm:ss
 setTimeout(displayTime, 1000);
 }
 
-
-
 displayTime();
-$("#saveBtn").on("click", function () {
+$(".saveBtn").on("click", function () {
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, text);
 }) 
 
-
-
-    // Get item from local storage if any
     $("#9 .description").val(localStorage.getItem("9"));
     $("#10 .description").val(localStorage.getItem("10"));
     $("#11 .description").val(localStorage.getItem("11"));
@@ -27,6 +22,9 @@ $("#saveBtn").on("click", function () {
     $("#16 .description").val(localStorage.getItem("16"));
     $("#17 .description").val(localStorage.getItem("17"));
 
+    function updateCss() {
+        $(".time-block").each(function () {
+        })
+    }
 
-
-
+    updateCss();
